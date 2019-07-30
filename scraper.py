b879d49dca1f3465e97f78416d1ba00e26e99d0f
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-07-30 13:01:31 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,8 @@ import urlparse
 import lxml.html
 
 # scrape_table function: gets passed an individual page to scrape
-def scrape_table(root):
-    rows = root.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley table"> and puts in list variable 'rows'
+def scrape_table(jim):
+    rows = jim.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley table"> and puts in list variable 'rows'
 #     My guess is it knows to make a list variable because either 'cssselect' function has that written in or it does it automatically because there are multiple table rows
 # If a class name has a space in it, replace this with a . --> "Trolley table" > "Trolley.table"
     for row in rows:
