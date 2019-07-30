185978bf4ca95594602188be2c107a077848fe7e
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-07-30 13:12:14 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,9 @@ import lxml.html
 
 # scrape_table function: gets passed an individual page to scrape
 def scrape_table(jim):
+# define the function and call the parameter it will use (aka the cake "filling") as 'jim' - or any other name
     rows = jim.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley table"> and puts in list variable 'rows'
+# tell the function what it will do with 'jim'
 #     My guess is it knows to make a list variable because either 'cssselect' function has that written in or it does it automatically because there are multiple table rows
 # If a class name has a space in it, replace this with a . --> "Trolley table" > "Trolley.table"
     for row in rows:
@@ -34,8 +36,9 @@ def scrape_and_look_for_next_link(starting_url):
     html = scraperwiki.scrape(starting_url)
     print html
     root = lxml.html.fromstring(html)
+# define the variable you plan to put into the scrape_table function as its argument (the actual "filling" e.g. custard or jam)
     scrape_table(root)
-
+# when you call scrape_table choose what specific argument you're replacing the parameter (jim) with (root) - ie. custard
 
 # ---------------------------------------------------------------------------
 # START HERE: define your starting URL - then 
